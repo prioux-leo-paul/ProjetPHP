50aeeb382ad2a694412eb7825b1625b0cca71c1a
priouxl <leo-paul.prioux@etu.umontpellier.fr>
2020-10-22 10:48:45 +0200

    Config

---
+++ model/Model.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+require_once '../config/Conf.php';
+/**
+ * Description of Model
+ *
+ * @author giaccagliam
+ */
+class Model {
+    
+    static public $pdo;
+    
+    public static function Init(){
+        $hostname = Conf::getHostname();
+        $database_name = Conf::getDatabase();
+        $login = Conf::getLogin();
+        $password = Conf::getPassword();
+        try {
+            self::$pdo = new PDO("mysql:host=$hostname;dbname=$database_name", $login, $password,
+                     array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));   
+            self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);        
+
+        } catch(PDOException $e) {
+            if (Conf::getDebug()){
+                echo $e->getMessage();
+            } else {
+                echo 'Une erreur est survenue <a href=""> retour a la page d\'accueil </a>';
+            }
+            die();
+        }
+    }
+}
+//Ceci est un test
+Model::Init();
+?>
