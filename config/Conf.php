32c57465226117bfc4e0d0875df828c1da896382
priouxl <leo-paul.prioux@etu.umontpellier.fr>
2020-10-30 23:03:40 +0100

    maj

--- config/Conf.php
+++ config/Conf.php
@@ -18,16 +18,16 @@ class Conf {
     static private $databases = array(
     // Le nom d'hote est webinfo
     // ou localhost sur votre machine
-    'hostname' => 'webinfo',
+    'hostname' => 'localhost',
     // A l'IUT, vous avez une BDD nommee comme votre login
     // Sur votre machine, vous devrez creer une BDD
     'database' => 'priouxl',
     // A l'IUT, c'est votre login
     // Sur votre machine, vous avez surement un compte 'root'
-    'login' => 'priouxl',
+    'login' => '',
     // A l'IUT, c'est votre mdp (INE par defaut)
     // Sur votre machine personelle, vous avez creez ce mdp a l'installation
-    'password' => '091094463JA'
+    'password' => ''
   );
    
   static public function getLogin() {
--- index.php
+++ index.php
@@ -1,4 +1,40 @@
-<?php
-require_once 'lib/File.php';
-require build_path(array('controller','routeur.php'));
-?>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="index.css">
+	<meta name="viewport" content="whidth=device-width,initial-scale=1">
+</head>
+<body>
+    <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+
+    <div class="wrapper">
+        <div class="sidebar">
+            <h2>Slide-Shop</h2>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Profile</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">portfolio</a></li>
+                <li><a href="#">Blogs</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Map</a></li>
+            </ul> 
+            <div class="social_media">
+              <a href="#"><i class="fab fa-facebook-f"></i></a>
+              <a href="#"><i class="fab fa-twitter"></i></a>
+              <a href="#"><i class="fab fa-instagram"></i></a>
+          </div>
+        </div>
+        <div class="main_content">
+            <?php
+                require_once 'lib/File.php';
+                require build_path(array('controller','routeur.php'));
+            ?>
+            
+        </div>
+    </div>
+
+</body>
+
+
+</html>
