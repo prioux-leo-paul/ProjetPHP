3e1338c1d309cc2e271e3f097d47ee4d19aa2814
dimitrii-benoit-iut <dimitrii.benoit@etu.umontpellier.fr>
2020-11-12 16:47:23 +0100

    Ajout verification : ?action est present dans la classe

--- controller/routeur.php
+++ controller/routeur.php
@@ -2,6 +2,9 @@
 require_once File::buildpath(array("controller","ControllerMembre.php"));
 // On recupère l'action passée dans l'URL
 $action = isset($_GET['action']) ? $_GET['action'] : 'Home';
+if (!in_array((isset($_GET['action']) ? $_GET['action'] : 'Home'),get_class_methods('ControllerMembre'))){
+    $action = 'Home';
+} 
 // Appel de la méthode statique $action de ControllerVoiture
 ControllerMembre::$action();
 ?>
